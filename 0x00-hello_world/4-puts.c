f063249505a9f3c7a33919335250ab2f07c71d60
Kwabena <kontorboampongjr@gmail.com>
2022-10-14 03:17:21 +0000

    White spaces removed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+* main- entry point
+* Return:0 if no errors otherwise return non-zero
+*/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
