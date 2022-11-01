3ac8a10f0aa862d4b0dadbd3229a7b74bbb3c2ca
Kwabena <kontorboampongjr@gmail.com>
2022-11-01 05:38:25 +0000

    works?

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,18 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*
+* print_diagsums- sum diagonal values
+* @a: pointer to array
+* @size: int
+* Return: sum of two sides
 */
 void print_diagsums(int *a, int size)
 {
-int i;
-long int sum = 0, sizem;
-sizem = size * size;
-for (i = 0; i < sizem; i += size + 1)
-sum += a[i];
-printf("%li, ", sum);
-sum = 0;
-for (i = size - 1; i <= sizem - size + 1; i += size - 1)
-sum += a[i];
-printf("%li\n", sum);
+	int i;
+	long int sum = 0, sizem;
+
+	sizem = size * size;
+	for (i = 0; i < sizem; i += size + 1)
+	{
+		sum += a[i];
+	}
+	printf("%li, ", sum);
+	for (i = size - 1; i <= sizem - size + 1; i += size - 1)
+	{
+		sum += a[i];
+	}
+	printf("%li\n", sum);
 }
