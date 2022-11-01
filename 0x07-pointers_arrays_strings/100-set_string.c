975f51d5c64773f2b9925d33c392626caaadbb8d
Kwabena <kontorboampongjr@gmail.com>
2022-11-01 05:45:03 +0000

    good

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,11 @@
-#indluce "main.h"
+#include "main.h"
 /**
-*
+* set_string - set str to char
+* @s: pointer to pointer to a char
+* @to: address of match
+* Return: void
 */
 void set_string(char **s, char *to)
 {
-*s = to;
+	*s = to;
 }
