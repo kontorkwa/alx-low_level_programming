5497b4df99a76a5765572867a0dca35cfb837daf
Kwabena <kontorboampongjr@gmail.com>
2022-11-01 03:54:56 +0000

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
-*
+* _strspn- get lenght of a prefix substring
+* @s: new location of byte
+* @accept: old location of byte
+* Return: 0
 */
-
 unsigned int _strspn(char *s, char *accept)
 {
-int i, k, counter = 0;
-for (i = 0; s[i] != '\0'; i++)
-if (counter != i)
-break;
-for (k = 0; accept[k] != '\0'; k++)
-{
-if (s[i] == accept[k])
-counter++;
-}
-}
-return (counter);
+	int i, k, counter = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (counter != i)
+		break;
+	}
+	for (k = 0; accept[k] != '\0'; k++)
+	{
+		if (s[i] == accept[k])
+		counter++;
+	}
+
+	return (counter);
 }
