ff8e7d270bcf8bd50216b32aa8682d93f5a5ca56
Kwabena <kontorboampongjr@gmail.com>
2022-11-01 03:34:59 +0000

    learning

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
-*
+* _strchr - finds a ch in str
+* @s: address of str
+* @c: ch to find
+* Return: 0
 */
-
 char *_strchr(char *s, char c)
 {
-do{
-if (*s == c)
-return (s);
-}
-while (*s++);
-return (0);
+	do	{
+		if (*s == c)
+		return (s);
+		}
+
+	while { (s++);
+	}
+	return (0);
 }
