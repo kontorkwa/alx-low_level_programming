167bd31824ed7394fcbc86315d16009f30925846
Kwabena <kontorboampongjr@gmail.com>
2022-11-01 03:23:36 +0000

    learning

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /**
-*
+* _memcpy - copies memory area
+* @dest: address of new memory area
+* @src: address of old memory area
+* @n: byte value to copy
+* Return: 0
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
